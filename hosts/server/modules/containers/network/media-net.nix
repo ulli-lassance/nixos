94f0293f6c26968fa3357195c89714ce1c27eef1
Lassance <ulli.lassance@gmail.com>
2026-03-20 14:07:27 -0300

    testing thing

--- hosts/server/modules/containers/network/media-net.nix
+++ hosts/server/modules/containers/network/media-net.nix
@@ -2,14 +2,22 @@
 
 {
   systemd.services."podman-network-media-net" = {
-    path = [ pkgs.podman ];
+    path = [
+      pkgs.podman
+      "/run/wrappers"
+    ];
+
+    after = [ "user@1000.service" ];
+    requires = [ "user@1000.service" ];
+
     serviceConfig = {
       Type = "oneshot";
       RemainAfterExit = true;
       User = vars.username;
     };
     environment = {
-      HOME = vars.homeDirectory; 
+      HOME = vars.homeDirectory;
+      XDG_RUNTIME_DIR = "/run/user/1000";
     };
     script = ''
       podman network exists media-net || podman network create media-net
