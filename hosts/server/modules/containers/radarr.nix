84e79c0e23122dae6367ddd5660e572fd6317efb
Lassance <ulli.lassance@gmail.com>
2026-03-20 15:20:30 -0300

    more container permissions...

--- hosts/server/modules/containers/radarr.nix
+++ hosts/server/modules/containers/radarr.nix
@@ -29,7 +29,10 @@
       ];
       ports = [ "127.0.0.1:7878:7878" ];
 
-      extraOptions = [ "--network=media-net" ];
+      extraOptions = [ 
+        "--network=media-net" 
+        "--userns=keep-id" 
+      ];
     };
   };
 
