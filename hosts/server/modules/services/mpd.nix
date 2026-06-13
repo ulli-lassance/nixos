ebc5a4e0d9e7ca7e3242d20a9768d5a4fd159f3f
ulli-lassance <john.lassance@gmail.com>
2026-06-13 01:09:56 -0300

    mpd permissions

--- hosts/server/modules/services/mpd.nix
+++ hosts/server/modules/services/mpd.nix
@@ -2,6 +2,7 @@
 {
   services.mpd = {
     enable = true;
+    user = config.settings.user.username; # so it can read music
     settings = {
       music_directory = "${config.settings.user.home}/music";
     };
