52c242de991df4a2d5d0b71dac10c3b340ba406b
ulli-lassance <john.lassance@gmail.com>
2026-06-13 01:47:05 -0300

    im dumb...

--- hosts/server/modules/services/mpd.nix
+++ hosts/server/modules/services/mpd.nix
@@ -19,9 +19,9 @@
     };
 
     openFirewall = true;
+  };
 
-    systemd.services.mpd.environment = {
-      XDG_RUNTIME_DIR = "/run/user/1000";
-    };
+  systemd.services.mpd.environment = {
+    XDG_RUNTIME_DIR = "/run/user/1000";
   };
 }
