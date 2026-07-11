afa3ae92c0f64d8ac1a6063cb6ffbc3cd58cd657
ulli-lassance <john.lassance@gmail.com>
2026-07-11 20:32:43 -0300

    adgaurd dns over https

--- hosts/server/modules/containers/adguard.nix
+++ hosts/server/modules/containers/adguard.nix
@@ -36,6 +36,18 @@
   services.nginx.virtualHosts."adguard.lan.${config.settings.server.domain}" = {
     useACMEHost = config.settings.server.domain;
     forceSSL = true;
+    http2 = true;
+
+    locations."/dns-query" = {
+      proxyPass = "http://127.0.0.1:3000/dns-query";
+
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      '';
+    };
+
     locations."/" = {
       proxyPass = "http://127.0.0.1:3000";
       proxyWebsockets = true;
