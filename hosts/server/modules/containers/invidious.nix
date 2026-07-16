a0fc801825a965c6a52bfe4193245005bc65f6be
ulli-lassance <john.lassance@gmail.com>
2026-07-16 16:27:24 -0300

    invidious rollback

--- hosts/server/modules/containers/invidious.nix
+++ hosts/server/modules/containers/invidious.nix
@@ -116,7 +116,11 @@ in
       labels = {
         "io.containers.autoupdate" = "registry";
       };
-      image = "quay.io/invidious/invidious:master";
+
+      # master branch
+      # image = "quay.io/invidious/invidious:master";
+
+      image = "quay.io/invidious/invidious:latest";
       podman.user = config.settings.user.username;
 
       environmentFiles = [ config.sops.templates."invidious.env".path ];
