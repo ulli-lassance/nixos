9113a2f8c0e0625268862f4a28424374604c4c23
ulli-lassance <john.lassance@gmail.com>
2026-07-19 20:48:10 -0300

    make feishin not use lan domain

--- hosts/server/modules/containers/feishin.nix
+++ hosts/server/modules/containers/feishin.nix
@@ -16,7 +16,7 @@
         SERVER_NAME = "nixos server";
         SERVER_LOCK = "true";
         SERVER_TYPE = "navidrome";
-        SERVER_URL = "https://navidrome.lan.${config.settings.server.domain}";
+        SERVER_URL = "https://navidrome.${config.settings.server.domain}";
         ANALYTICS_DISABLED = "true";
       };
 
