b086d931d0aa8db39831010eb0f3deb709407141
ulli-lassance <john.lassance@gmail.com>
2026-07-19 21:19:39 -0300

    jellyfin proxy fix

--- hosts/server/modules/containers/jellyfin.nix
+++ hosts/server/modules/containers/jellyfin.nix
@@ -42,12 +42,33 @@
 
     useACMEHost = config.settings.server.domain;
     forceSSL = true;
+
     locations."/" = {
       proxyPass = "http://127.0.0.1:8096";
       proxyWebsockets = true;
       extraConfig = ''
         proxy_buffering off;
         client_max_body_size 20M;
+
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
+      '';
+    };
+
+    locations."/socket" = {
+      proxyPass = "http://127.0.0.1:8096";
+      proxyWebsockets = true;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+        proxy_set_header X-Forwarded-Protocol $scheme;
+        proxy_set_header X-Forwarded-Host $http_host;
       '';
     };
   };
