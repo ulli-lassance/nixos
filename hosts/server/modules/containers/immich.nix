83a8c37388226e0d7f86032417fb47f36057db55
ulli-lassance <john.lassance@gmail.com>
2026-07-22 20:55:16 -0300

    immich proxy fixes

--- hosts/server/modules/containers/immich.nix
+++ hosts/server/modules/containers/immich.nix
@@ -193,11 +193,17 @@ in
       proxyWebsockets = true;
       extraConfig = ''
         proxy_buffering off;
+        proxy_request_buffering off;
+        
         client_max_body_size 50000M;
+
+        client_body_buffer_size 1024k;
+
         proxy_set_header X-Forwarded-Protocol $scheme;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
         proxy_read_timeout 600s;
         proxy_send_timeout 600s;
         proxy_connect_timeout 600s;
