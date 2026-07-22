2e3565a890acd64b8af698eefb64513c0f402a2d
ulli-lassance <john.lassance@gmail.com>
2026-07-22 03:32:42 -0300

    fix nginx warn

--- hosts/server/modules/services/nginx.nix
+++ hosts/server/modules/services/nginx.nix
@@ -33,6 +33,10 @@
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
     recommendedOptimisation = true;
+    appendHttpConfig = ''
+      proxy_headers_hash_max_size 1024;
+      proxy_headers_hash_bucket_size 128;
+    '';
   };
 
   users.users.nginx.extraGroups = [ "acme" ];
