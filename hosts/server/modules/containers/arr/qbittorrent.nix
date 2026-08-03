c84d764131fcdc461fc2105e2720aab72e2f4de4
ulli-lassance <john.lassance@gmail.com>
2026-08-03 04:01:53 -0300

    qbittorrent vpn

--- hosts/server/modules/containers/arr/qbittorrent.nix
+++ hosts/server/modules/containers/arr/qbittorrent.nix
@@ -1,19 +1,67 @@
 { config, ... }: {
+
+  sops.secrets.protonvpn_wg_key = {
+  };
+
+  sops.templates."gluetun-protonvpn.env".content = ''
+    WIREGUARD_PRIVATE_KEY=${config.sops.placeholder.protonvpn_wg_key}
+  '';
+
   systemd.tmpfiles.rules = [
     "d ${config.settings.server.volumeDirectory}/qbittorrent 0755 ${config.settings.user.username} users -"
     "d ${config.settings.server.volumeDirectory}/qbittorrent/config 0755 ${config.settings.user.username} users -"
-    
+    "d ${config.settings.server.volumeDirectory}/gluetun 0755 root root -"
     "d ${config.settings.user.home}/downloads 0755 ${config.settings.user.username} users -"
   ];
 
   virtualisation.oci-containers.containers = {
+    gluetun = {
+      autoStart = true;
+      image = "qmcgaw/gluetun";
+
+      ports = [
+        "127.0.0.1:8080:8080"
+        "6881:6881"
+        "6881:6881/udp"
+      ];
+
+      environmentFiles = [
+        config.sops.templates."gluetun-protonvpn.env".path
+      ];
+
+      environment = {
+        VPN_SERVICE_PROVIDER = "protonvpn";
+        VPN_TYPE = "wireguard";
+        SERVER_COUNTRIES = "Brazil";
+        VPN_PORT_FORWARDING = "on";
+
+        VPN_PORT_FORWARDING_UP_COMMAND = ''
+          /bin/sh -c 'wget -O- -nv --retry-connrefused --post-data "json={\"listen_port\":{{PORT}},\"current_network_interface\":\"{{VPN_INTERFACE}}\",\"random_port\":false,\"upnp\":false}" http://127.0.0.1:8080/api/v2/app/setPreferences'
+        '';
+
+        VPN_PORT_FORWARDING_DOWN_COMMAND = ''
+          /bin/sh -c 'wget -O- -nv --retry-connrefused --post-data "json={\"listen_port\":0,\"current_network_interface\":\"lo\"}" http://127.0.0.1:8080/api/v2/app/setPreferences'
+        '';
+      };
+
+      volumes = [
+        "${config.settings.server.volumeDirectory}/gluetun:/gluetun"
+      ];
+
+      extraOptions = [
+        "--cap-add=NET_ADMIN"
+        "--device=/dev/net/tun:/dev/net/tun"
+        "--network=arr-net"
+      ];
+    };
+
     qbittorrent = {
       autoStart = true;
+      dependsOn = [ "gluetun" ];
       labels = {
         "io.containers.autoupdate" = "registry";
       };
       image = "lscr.io/linuxserver/qbittorrent:latest";
-
       podman.user = config.settings.user.username;
 
       environment = {
@@ -25,17 +73,17 @@
         "${config.settings.server.volumeDirectory}/qbittorrent/config:/config:U"
         "${config.settings.user.home}/downloads:/downloads"
       ];
-      ports = [
-        "127.0.0.1:8080:8080"
-        "6881:6881"
-        "6881:6881/udp"
-      ];
 
       extraOptions = [
-        "--network=arr-net"
+        "--network=container:gluetun"
         "--userns=keep-id"
       ];
     };
+  };
+
+  systemd.services."podman-gluetun" = {
+    after = [ "podman-network-arr-net.service" ];
+    requires = [ "podman-network-arr-net.service" ];
   };
 
   systemd.services."podman-qbittorrent" = {
