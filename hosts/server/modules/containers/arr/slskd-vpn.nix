eefeafee736bbd68b476ef5ca848c1a0d11ab22b
ulli-lassance <john.lassance@gmail.com>
2026-08-07 02:32:30 -0300

    simple move music helper script

--- hosts/server/modules/containers/arr/slskd-vpn.nix
+++ hosts/server/modules/containers/arr/slskd-vpn.nix
@@ -1,4 +1,50 @@
-{ config, ... }: {
+{ config, pkgs, ... }:
+
+let
+  srcDir = "${config.settings.server.containerData}/soulseek/complete";
+  destDir = "${config.settings.server.containerData}/media/music/unsorted";
+
+  moveSoulseekMusic = pkgs.writeShellApplication {
+    name = "move-soulseek-music";
+
+    runtimeInputs = [
+      pkgs.coreutils
+    ];
+
+    text = ''
+      SRC="${srcDir}"
+      DEST="${destDir}"
+
+      if [ ! -d "$SRC" ]; then
+        echo "Error: Source directory '$SRC' does not exist." >&2
+        exit 1
+      fi
+
+      if [ ! -d "$DEST" ]; then
+        echo "Error: Destination directory '$DEST' does not exist." >&2
+        exit 1
+      fi
+
+      shopt -s nullglob dotglob
+      files=("$SRC"/*)
+
+      if [ ''${#files[@]} -eq 0 ]; then
+        echo "No music files found in '$SRC' to move."
+        exit 0
+      fi
+
+      echo "Moving ''${#files[@]} item(s) from '$SRC' to '$DEST'..."
+      mv "''${files[@]}" "$DEST"/
+      echo "Done!"
+    '';
+  };
+in
+
+{
+  environment.systemPackages = [
+    moveSoulseekMusic
+  ];
+
   systemd.tmpfiles.rules = [
     "d ${config.settings.server.volumeDirectory}/slskd-vpn 0755 ${config.settings.user.username} users -"
     "d ${config.settings.server.volumeDirectory}/slskd-gluetun 0755 ${config.settings.user.username} users -"
