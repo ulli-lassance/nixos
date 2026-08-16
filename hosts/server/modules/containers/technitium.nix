7c15c0c6a104516c3a4adf1ae59db7f1d72380e3
ulli-lassance <john.lassance@gmail.com>
2026-08-16 02:24:23 -0300

    a

--- hosts/server/modules/containers/technitium.nix
+++ hosts/server/modules/containers/technitium.nix
@@ -60,9 +60,7 @@
       proxyPass = "http://127.0.0.1:8053/dns-query";
 
       extraConfig = ''
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_http_version 1.1;
       '';
     };
 
